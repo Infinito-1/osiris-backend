da6f559522cf963ae5c58f1fa0f8d0e0bc50bb71
Lari <larissateixeira998@gmail.com>
2026-02-23 23:31:39 -0300

    estrutura base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProdService } from './data/prod.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
+    }),
+
+  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
---
+++ src/grupo/entities/grupo.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
+
+@Entity({name: 'grupos'})
+export class Grupo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({length: 70, nullable: false})
+  nome: string;
+
+  
+}
