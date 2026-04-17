4e47865acb25dcbb90f2d0fd903cc12c36a4742d
deysemedeiros <deysebmsoares@gmail.com>
2026-04-17 16:24:32 -0300

    Modifiquei o acesso da entidade

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { DevService } from './data/dev.service';
 import { GrupoModule } from './grupo/grupo.module';
+import { EmpreendedorModule } from './empreendedor/empreendedor.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { GrupoModule } from './grupo/grupo.module';
       imports: [ConfigModule],
     }),
     GrupoModule,
+    EmpreendedorModule,
   ],
   controllers: [AppController],
   providers: [],
