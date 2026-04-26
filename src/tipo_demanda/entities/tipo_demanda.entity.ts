608e6a67e8fca807616be6b1267413f5d58efa0c
Lari <larissateixeira998@gmail.com>
2026-04-26 20:16:58 -0300

    padroniza tipo de demanda

--- src/tipo_demanda/entities/tipo_demanda.entity.ts
+++ src/tipo_demanda/entities/tipo_demanda.entity.ts
@@ -3,10 +3,10 @@ import { IsNotEmpty } from 'class-validator';
 
 @Entity({ name: 'tipo_demanda' })
 export class TipoDemanda {
-  @PrimaryGeneratedColumn()
-  idTipoDemanda!: number;
+  @PrimaryGeneratedColumn({ name: 'tip_int_id' })
+  tipIntId!: number;
 
   @IsNotEmpty()
-  @Column({ length: 100, nullable: false })
-  nome!: string;
+  @Column({ name: 'tip_str_tipStrNome', length: 100, nullable: false })
+  tipStrNome!: string;
 }
